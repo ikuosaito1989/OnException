4b9a9174255673ad11332c1d89cc1c9d9df7b033
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-03-01 14:59:05 +0900

    first commit

---
+++ OnException/OnException/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnException.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            throw new Exception("HomeController");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        [Route("error")]
+        public ActionResult Error(string aspxerrorpath = "")
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+    }
+
+    public class BaseController : Controller
+    {
+        /// <summary>
+        /// MVCエラー
+        /// </summary>
+        /// <param name="filterContext"></param>
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            Console.WriteLine("ControllerError");
+        }
+    }
+}
---
+++ OnException/OnException/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace OnException
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            // Application_Error発生
+            //throw new Exception("Application_BeginRequest");
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Console.WriteLine("Application_Error");
+            Server.ClearError();
+            Server.Transfer("/Error");
+            return;
+        }
+    }
+}
